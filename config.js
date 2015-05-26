214706db64e2b064d5e41a9bd31e566122dd0aef
joeYeager <jayeag00@gmail.com>
2015-05-25 19:48:19 -0700

    Set up transpiling using the code Ladinu had in the server gulpfile.  Gulp will now watch all of the .ts files in the project and transpile them on save.  Added browser-sync to gulp.  This will serve all of the files in a defined directory on localhost:3000.  It will watch the files in that directory and when they are saved, it will reload the page so you can see your changes quicker.  It is currently only watching two garbage directorys (html & css) that I made up until we have the project structure more hashed out.  Then we can fill them in with the real paths.  Added a few dependencies, so make sure to npm install, bower install, and tsd install.  Updated the .gitignore to exclude the built directory created by transpiling as well as the typings directory created by tsd.

--- config.js
+++ config.js
@@ -1,3 +1,16 @@
+var path = require('path');
+var package = require('./package.json');
+var tsConfig = require('./tsconfig.json');
+var tsd = require('./tsd.json');
+
 module.exports = {
-  
+  'tsConfigFile': 'tsconfig.json',
+  'tsconfig': tsConfig,
+  'tsd': tsd,
+  'package': package,
+  'tsOutDir': tsConfig.compilerOptions.outDir,
+  'tsFilesGlob': tsConfig.filesGlob,
+  'bsPort': 3000,
+  'htmlDir': './html/',
+  'cssDir' : './css/'
 }
--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,41 @@
 var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
+var spawn  = require('child_process').spawn;
 $.del = require('del');
+var config = require('./config.js')
+
+var ts           = $.typescript;
+var tsProject    = ts.createProject(config.tsConfigFile);
+var failOnErrors = false;
+$.ts = $.typescript;
+
+// Require and Initialize browser sync with our configurtion
+var browserSync  = require('browser-sync').create();
+
+browserSync.init({
+        server: {
+            baseDir: config.htmlDir,
+        },
+        port: config.bsPort
+});
+
+var handleError = function(err) {
+  if (failOnErrors) {
+    $.util.log(err.message);
+    process.exit(1);
+  }
+}
+
+gulp.task('transpile-ts2js', function () {
+  return tsProject.src()
+                  .pipe($.ts(tsProject))
+                  .on('error', handleError)
+                  .js
+                  .pipe(gulp.dest(config.tsOutDir));
+});
+
+gulp.task('default', function() {
+   gulp.watch(config.tsFilesGlob, ['transpile-ts2js']);
+   gulp.watch(config.htmlDir, browserSync.reload);
+   gulp.watch(config.cssDir, browserSync.reload);
+});
