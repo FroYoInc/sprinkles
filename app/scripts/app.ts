7e4679831aaa21f696e4f4ce8bc31197623933de
jushuck <jushuck@pdx.edu>
2015-06-17 21:34:15 -0700

    Angular doesn't spit out error messages

--- app/scripts/app.ts
+++ app/scripts/app.ts
@@ -2,18 +2,10 @@
 /// <reference path='..\libs\jquery\jquery.d.ts' />
 
 /// <reference path='components/home/homeController.ts' />
-/// <reference path='components/signup/signupController.ts' />
 
 
-/**
-  *
-  * Main app module
-  * @type {angular.Module}
-  */
+angular.module('app', ['app.controllers'])
 
-  module sprinkles {
-  'use strict';
- var app = angular.module('app', [])
-    .controller('HomeCtrl', HomeCtrl)
-    //.controller('SignupCtrl', SignupCtrl)
+module app {
+    export var controllers = angular.module('app.controllers',[]);
 }
