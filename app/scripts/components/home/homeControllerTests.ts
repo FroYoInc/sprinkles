dc497723cc21de39652e4df790bc41671a2e22ff
Dominic Schira <domshyra@gmail.com>
2015-07-07 11:37:41 -0700

    Updated comments

--- app/scripts/components/home/homeControllerTests.ts
+++ app/scripts/components/home/homeControllerTests.ts
@@ -1,6 +1,15 @@
 /// <reference path="../../app.ts"/>
 
-// interface used to get a get user from their email and password during sign inmodule SignIn{
+// This is a file that shows the process of elmination I went through to get the scope to work properly. It may be helpful to look at what I tried in order to save time trying to do that same thing. 
+
+// It appears that if any data is tried to get passed into the construtor none of the home view will load. Therefor trying to create a fake user in the construcot method seemes to not be a solution to this problem. 
+
+//None of this code will work but if you uncomment 
+            // $scope.testUser.Email = "t@test.com" 
+            // and comment out the 
+            // newUser.Email = "test@test.com";
+            // this.testUser.Email = newUser.Email;
+// you will notice the same error of the componets not loading 
 
 interface User {
     Email: string;
@@ -10,42 +19,28 @@ interface User {
 interface Scope {
   events: any;
 }
-class homeController {
+class homeController { //removed ng.Scope and still worked
     
     testUser: User;
 
     // if any fake user creation is done during the cunstructors main block none of the home page will load propperly 
     //There for this proccess also does not work 
+
+
     // newUser: User is passed into the constructer so when we call the constructer we can create a user, but idk how to pass in the scope on the new homeController call
     constructor ($scope: Scope, newUser: User) {
-        $scope.events = this;
+        $scope.events = this; //is needed in order to connect the controller and the view 
         // create a fake user for testing purposes 
-        // $scope.testUser.Email = "t@test.com" // doesn't work so I tried it with a newUser constructer
+        // $scope.testUser.Email = "t@test.com" // doesn't work. This will make the componets of the home view never load
         newUser.Email = "test@test.com";
         this.testUser.Email = newUser.Email;
         }    
 
         clickMe () {
-            console.log(this.testUser.Email); 
+            console.log(this.testUser.Email); // if you uncomment $scope.testUser.Email = "t@test.com" you will need to change the log
         }
     }
-    // $http.get('http://localhost:8080/users/login?email="higgs@lhc.com"&password="1234"'
 
 
 app.controllers.controller('homeController', homeController);
 
-
- /*
-        $scope.createTestUser = function () {
-            var newUser = new UserModel.User();
-            newUser.Email = $scope.newUserEmail;
-            newUser.Pass = $scope.newUserPass; //Password is a reserved word
-            
-        }; */
-
-        /*
-        login(product: UserModel.User, successCallback: Function): void {
-        this.httpService.post('http://localhost:8080/users/login?email="higgs@lhc.com"&password="1234"', product).success(function () {
-            successCallback();
-        });
-        */
