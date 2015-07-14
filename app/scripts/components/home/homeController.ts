eeb6427837c1f6480f8f1167bf9c82ebe4d581c0
Dominic Schira <domshyra@gmail.com>
2015-07-13 20:59:34 -0700

    Added alerts to deal with sign in errors

--- app/scripts/components/home/homeController.ts
+++ app/scripts/components/home/homeController.ts
@@ -27,8 +27,34 @@ module Home {
 
                 console.log(user.email);
                 console.log(user.password);
-                $http.post('http://localhost:3000/api/users/login', user).success(function (data) {
+                $http.post('http://localhost:3000/api/users/login', user).
+                    success(function (data) {
                     console.log(data);
+                }).
+                    error(function (data, status) {
+                        console.log("status is" + status);
+                        // Bad Request. Invalid or missing parameters.
+                        if (status == 400){ //
+                            $('#badRequest').css('visibility','visible').fadeIn();
+                        }
+                        // Unauthorized attempt. User and password combination not found.
+                        else if (status == 401){
+                            $('#notAuthorized').css('visibility','visible').fadeIn();
+                        } 
+                        // The user account has not been activated.
+                        else if (status == 403){ //
+                            $('#accountNotActivated').css('visibility','visible').fadeIn();
+                        }
+                        // The user account is locked.
+                        else if (status == 423){ //
+                            $('#accountIsLocked').css('visibility','visible').fadeIn();
+                        }
+                        // Internal Server error.
+                        else if (status == 500){ //
+                            $('#internalError').css('visibility','visible').fadeIn();
+                        }
+
+
                 });
 
             };
