da63566617a508aafd07c0d71a13153bf8cf5c39
Justin Shuck <Justin.Shuck@cdk.com>
2015-07-16 00:02:26 -0700

    Adding a working version of the sign-in. A few bugs need to be addressed along with a testing suite.

--- app/scripts/components/signup/signupController.ts
+++ app/scripts/components/signup/signupController.ts
@@ -11,31 +11,40 @@ module Signup {
         firstName: string;
         lastName: string;
         events: any;
+        usernameerror: boolean;
+        emailExistsError: boolean;
+        emailDomainError: boolean;
+        userpassError: boolean;
+
         signupUser: Function;
         checkPasswords: Function;
         signup: Function;
         navClass: Function;
         loadSignup: Function;
-
-        $apply: any;
-        $watch: any;
-        $parent: any;
-        $emit: any;
     }
     export class Controller {
 
         constructor ($scope: Scope, $location, $http: any, $localStorage) {
             $scope.events = this;
+
+            //Adds the information into a temp local storage.
+            //This gets cleared after the user signs up.
             $scope.userEmail = $localStorage.email;
             $scope.firstName = $localStorage.fname;
             $scope.lastName = $localStorage.lname;
-            /*$scope.userEmail = localStorage.getItem('userEmail');
-            $scope.firstName = localStorage.getItem('firstName');
-            $scope.lastName = localStorage.getItem('lastName');*/
 
-            // populate the data when sign in is clickeds
+            //Helper function that hides error messages
+            var resetErrors = function() {
+              $scope.usernameerror = false;
+              $scope.emailExistsError = false;
+              $scope.emailDomainError = false;
+              $scope.userpassError = false;
+            }
+            resetErrors();
+
+            // Posts the user data
             $scope.signupUser = function () {
-
+              //Set the post data before we make the call
               var postData = {
                 userName: $scope.userName,
                 firstName: $scope.firstName,
@@ -43,27 +52,44 @@ module Signup {
                 email: $scope.userEmail,
                 password: $scope.userPass1
               };
-              console.log(postData);
-                $http.post('http://localhost:3000/api/users', postData).success(function(data, status, headers, config) {
-                  console.log(data);
+
+              resetErrors();
+              $http.post('http://localhost:3000/api/users', postData).success(function(data, status, headers, config) {
+                  //clear the localstorage
+                  $localStorage.$reset();
+
+                  //re-route and have popup show
+                  $location.url('/home');
+                  $('#accountmade').css('visibility','visible').fadeIn();
+
                 }).error(function(data, status, headers, config) {
-                  console.log('error');
-                  console.log(data);
+                  //Email exists
+                  if(status == 409 && ((data.message).includes("EmailValidationException:"))){
+                      $scope.emailExistsError = true;
+                  }
+                  //Bad email domain
+                  if(status == 400 && ((data.message).localeCompare("EmailValidationException: The email address's domain is not allowed") == 0)){
+                      $scope.emailDomainError = true;
+                  }
+                  //username exists
+                  else if( status == 409 && ((data.message).localeCompare("UserExistException: user already exist") == 0)){
+                      $scope.usernameerror = true;
+                  }
+                  //no password or username entered
+                  else if( status == 500 && data.message.includes("BcryptHashError")){
+                      $scope.userpassError = true;
+                  }
+
                 });
             };
 
+            // Loads the signup page
             $scope.loadSignup = function (userEmail, firstName, lastName) {
-                try{
-                  $localStorage.email = userEmail;
-                  $localStorage.fname = firstName;
-                  $localStorage.lname = lastName;
-                /*localStorage.setItem('userEmail', userEmail);
-                localStorage.setItem('firstName', firstName);
-                localStorage.setItem('lastName', lastName);*/
-              } catch(err){
-                console.log(err);
-              }
-                  $location.url('/signup');
+              //Sets the local storage from the re-routed source
+              $localStorage.email = userEmail;
+              $localStorage.fname = firstName;
+              $localStorage.lname = lastName;
+              $location.url('/signup');
              };
         }
 
