72c948b7c28cdb8f0016864fe8ff02c86448b616
Justin Shuck <Justin.Shuck@cdk.com>
2015-07-18 17:38:19 -0700

    Fixing conflicts with dashbord

--- app/scripts/app.ts
+++ app/scripts/app.ts
@@ -1,5 +1,3 @@
-/// <reference path='..\libs\angular\angular.d.ts' />
-/// <reference path='..\libs\jquery\jquery.d.ts' />
 /// <reference path='..\libs\angular\angular-route.d.ts'/>
 
 /// <reference path='components/home/homeController.ts' />
