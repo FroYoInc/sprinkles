e8402a35cc07c9f4bc50cba18f251e0d2e0940b5
Dominic Schira <domshyra@gmail.com>
2015-07-24 19:29:17 -0700

    Added edit carpool conntroller and view

---
+++ app/scripts/components/carpools/editCarpoolController.ts
@@ -0,0 +1,40 @@
+/// <reference path="../../app.ts"/>
+
+// interface used to display a list of carpools, edit user profile and create a carpool
+module Dashboard_Carpools_Edit {
+
+    export interface Scope {
+        carpoolId: string;
+        carpoolName: string;
+        carpoolDescription: string;
+        editCarpool: Function;
+    }
+    export class Controller {
+
+    	constructor ($scope: Scope, $http: any, $location: any,  $localStorage) {
+
+        $scope.carpoolId = $localStorage.carpoolId;
+        //Default Values
+
+        //Populates the Carpool list
+        $scope.editCarpool = function() {
+
+           $http.put('http://localhost:3000/api/carpools/', $scope.carpoolId ).success(function(data, status, headers, config) {
+                console.log($scope.carpoolId);
+             }).error(function(data, status, headers, config) {
+               //500 server error
+               if(status == 500){
+                 window.scrollTo(0,0);
+                   $('#internalError').css('visibility','visible').fadeIn();
+               }
+               if(status == 404) {
+                 window.scrollTo(0,0);
+                   $('#notFound').css('visibility','visible').fadeIn();
+               }
+             });
+        }
+
+    	}
+    }
+}
+app.controllers.controller('Dashboard_Carpools_Edit.Controller', Dashboard.Controller);
