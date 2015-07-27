3aa0d35cfb8b8397a443c22f38c7d98cedd0dbb6
alou-saechao <lou_saechao@Lous-MacBook-Air.local>
2015-07-27 16:21:47 -0700

    added view all requests of all carpools the user is in

--- app/scripts/components/Request/responseController.ts
+++ app/scripts/components/Request/responseController.ts
@@ -5,6 +5,8 @@ module Response {
 
   export interface Scope {
     loadRequestView: Function;
+    carpoolRequests: Function;
+    viewRequests: Function;
     users:any;
     emptyUsers:any;
     addUser:Function;
@@ -15,7 +17,9 @@ module Response {
   }
   export class Controller {
 
-    constructor($scope:Scope, $location) {
+    constructor($scope:Scope, $location, $http: any) {
+
+      /* Hard coded data to test in earlier implementation
 
       $scope.users = [{'name':'Lou','name2':'Saechao','name3':'alou'},
         {'name':'Justin','name2':'Shuck','name3':'jshuck'},
@@ -23,10 +27,30 @@ module Response {
 
       $scope.emptyUsers = [];
 
-
       $scope.addUser = function(){
         $scope.emptyUsers.push({'name':$scope.name,'name2':$scope.name2,'name3':$scope.name3})
       };
+
+     */
+
+      $scope.viewRequests = function(){
+        $http.get('http://localhost:3000/api/carpools/requests').success(function(data, status, headers, config) {
+          $scope.carpoolRequests = data;
+          console.log('success!')
+        }).error(function(data, status, headers, config){
+          if(status == 401){
+            console.log('Unauthorized attempt, user must be logged in.');
+          }
+          else if(status == 404){
+            console.log('user not found');
+          }
+          else if(status == 500){
+            console.log('Internal Server error. Unable to make request')
+          }
+        });
+      };
+
+
 
 
       $scope.loadRequestView = function() {
