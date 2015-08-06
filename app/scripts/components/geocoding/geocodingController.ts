a98009644a50b748a2ed33cf276f955112d963d8
joeYeager <jayeag00@gmail.com>
2015-08-05 18:18:28 -0700

    Initial commit for branch

---
+++ app/scripts/components/geocoding/geocodingController.ts
@@ -0,0 +1,39 @@
+module GeoCoding {
+
+    export interface Scope {
+        geocodeAddress: Function;
+    };
+
+    export interface GeoCode{
+        long: number;
+        lat: number;  
+    }
+    export class Controller {
+        constructor($scope: Scope, $location, $http: any, $cookies: any, ConfigService: any){
+            
+            $scope.geocodeAddress = (address, cb: (success:GeoCode) => void) => {
+                $http.get( ConfigService.mapsApi + address + '&key=' + ConfigService.key)
+                  .success( (data) => {
+                    var Geo:GeoCode = null;
+                    if(data.results[0] !== undefined){
+                        var geo = data.results[0].geometry.location;
+                        Geo = {
+                            long : geo.lng,
+                            lat: geo.lat
+                        };
+                    }
+
+                    cb(Geo);
+                  })
+                  .error((data, status) => {
+                    cb(null);
+                  });
+            }
+        };
+
+    };
+
+};
+app.controllers.controller('GeoCoding.Controller', GeoCoding.Controller);
+
+
